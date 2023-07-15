3aa8c9da5c0b668f169d9a73f15aa58f0290aa26
youngyin <57608725+youngyin@users.noreply.github.com>
2023-07-15 23:07:51 +0900

    project init
    IntelliJ IDE에서 Spring Initializr를 이용해 프로젝트를 생성해보자.

---
+++ src/main/java/com/yin/catsearch/CatSearchApplication.java
@@ -0,0 +1,13 @@
+package com.yin.catsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatSearchApplication.class, args);
+    }
+
+}
